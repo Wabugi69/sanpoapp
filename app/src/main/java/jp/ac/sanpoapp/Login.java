cf32cba378d012945bcac85cfd3164bcc2d42a68
Wabugi69 <kevin.abercrombie@wsu.edu>
2025-06-19 15:49:58 +0900

    app is communicating with server again

--- app/src/main/java/jp/ac/sanpoapp/Login.java
+++ app/src/main/java/jp/ac/sanpoapp/Login.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -118,6 +119,8 @@ public class Login extends AppCompatActivity {
                 while ((line = br.readLine()) != null) {
                     response.append(line.trim());
                 }
+                br.close();
+
                 JSONObject json = new JSONObject(response.toString());
                 String finalResponse = json.getString("message");
 
